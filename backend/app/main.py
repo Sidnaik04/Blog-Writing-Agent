0afe34e5279a88d2b431419b43942e7a57c0e40e
Sidnaik04 <naiksidhant04@gmail.com>
2026-03-29 18:37:21 +0530

    final step done

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,7 +13,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://blog-writing-agent-m99p.vercel.app/login"],
+    allow_origins=["https://blog-writing-agent-m99p.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
